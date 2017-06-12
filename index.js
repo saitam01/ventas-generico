364e82ab186f343ce7c05820e32a09ab06b0b8c1
saitam01 <macenlared@gmail.com>
2017-06-11 20:25:01 -0400

    Navegabilidad

--- index.js
+++ index.js
@@ -13,8 +13,31 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/shop', function(request, response) {
+  response.render('pages/shop');
+});
+
+app.get('/product-details', function(request, response) {
+    response.render('pages/product-details');
+});
+
+app.get('/login', function(request, response) {
+    response.render('pages/login');
+});
+
+app.get('/404', function(request, response) {
+    response.render('pages/404');
+});
+
+app.get('/contact-us', function(request, response) {
+    response.render('pages/contact-us');
+});
+
+app.get('/cart', function(request, response) {
+    response.render('pages/cart');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
 
